5b09cd5a80450004c78a8646d3ceebcb0b064a10
cganzon <crganzon@gmail.com>
2022-07-11 18:32:37 -0700

    add js for the new project input and buttons

--- src/pageComponts/sidebar/sidebar.js
+++ src/pageComponts/sidebar/sidebar.js
@@ -3,10 +3,39 @@ import "./sidebar.css";
 const sidebar = () => {
   const sidebar = document.createElement("section");
   const sidebarTitle = document.createElement("h2");
+  const projects = document.createElement("div");
+  const newProjectButton = document.createElement("button");
+  const createProject = document.createElement("div");
+  const createProjectInput = document.createElement("input");
+  const createProjectButton = document.createElement("button");
+  const cancelProjectButton = document.createElement("button");
 
   sidebarTitle.textContent = "Projects";
 
-  sidebar.append(sidebarTitle);
+  projects.classList.add("projects");
+
+  newProjectButton.classList.add("new-project-button");
+  newProjectButton.textContent = "New Project";
+
+  createProject.classList.add("create-project");
+
+  createProjectInput.classList.add("create-project-input");
+  createProjectInput.setAttribute("type", "text");
+  createProjectInput.setAttribute("placeholder", "Enter project name");
+
+  createProjectButton.classList.add("create-new-project");
+  createProjectButton.textContent = "Create";
+
+  cancelProjectButton.classList.add("cancel-new-project");
+  cancelProjectButton.textContent = "Cancel";
+
+  createProject.append(
+    createProjectInput,
+    createProjectButton,
+    cancelProjectButton
+  );
+
+  sidebar.append(sidebarTitle, projects, newProjectButton, createProject);
 
   return sidebar;
 };
