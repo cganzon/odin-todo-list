930d8f450e1a8fde6ca37ad4b3846b214182f6d8
cganzon <crganzon@gmail.com>
2022-07-15 20:05:01 -0700

    display any projects from local storage in the sidebar

--- src/initialPageLoad.js
+++ src/initialPageLoad.js
@@ -1,10 +1,14 @@
+import displayProject from "./dom/displayProject";
 import header from "./pageComponts/header/header";
 import sidebar from "./pageComponts/sidebar/sidebar";
 
 const initialPageLoad = () => {
   document.body.append(header(), sidebar());
+  
   if (localStorage.projects) {
     console.log(JSON.parse(localStorage.projects));
+    const currentProjects = JSON.parse(localStorage.getItem("projects"));
+    currentProjects.forEach((project) => displayProject(project));
   } else {
     localStorage.projects = JSON.stringify([]);
   }
